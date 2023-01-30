98b576110baa94c682323beb23c74fa161d1d3bf
maishinsu <mkzk.mail@gmail.com>
2023-01-30 15:33:17 -0300

    refactor: move config block to separate function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,12 @@
+mod db;
 mod routes;
-mod db; 
 mod schema;
 
-use routes::adventurer;
 use db::AdventurersGuild;
-use rocket::Config;
-use std::{env, collections::HashMap};
 use dotenvy::dotenv;
+use rocket::{figment::Figment, Config};
+use routes::adventurer;
+use std::{collections::HashMap, env};
 
 #[macro_use]
 extern crate rocket;
@@ -17,22 +17,23 @@ extern crate rocket_sync_db_pools;
 #[macro_use]
 extern crate diesel;
 
-#[launch]
-fn app() -> _ {
-    dotenv().ok();
-
-    tracing_subscriber::fmt::init();
-
+fn get_config() -> Figment {
     let mut dbs = HashMap::new();
     let mut db_config = HashMap::new();
     let db_url = env::var("DATABASE_URL").expect("missing DATABASE_URL in .env file.");
     db_config.insert("url", db_url);
     dbs.insert("adventurers_guild", db_config);
 
-    let figment = Config::figment()
-        .merge(("databases", dbs));
+    Config::figment().merge(("databases", dbs))
+}
 
-    rocket::custom(figment)
+#[launch]
+fn app() -> _ {
+    dotenv().ok();
+
+    tracing_subscriber::fmt::init();
+
+    rocket::custom(get_config())
         .attach(AdventurersGuild::fairing())
         .mount("/adventurer", adventurer::get_routes())
 }
